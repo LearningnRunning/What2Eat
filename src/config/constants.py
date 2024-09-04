886e3648320016e6b5fd697e562c960015f2eee3
LearningnRunning <rocki.k1208@gmail.com>
2024-09-04 10:01:39 +0900

    FIX path error

--- src/config/constants.py
+++ src/config/constants.py
@@ -1,12 +1,13 @@
 import streamlit as st
 
-LOGO_IMG_PATH = '../static/img/what2eat-logo-middle.png'
-LOGO_SMALL_IMG_PATH = '../static/img/what2eat-logo-small.png'
-LOGO_TITLE_IMG_PATH = '../static/img/what2eat-word-logo-small.png'
+LOGO_IMG_PATH = './static/img/what2eat-logo-middle.png'
+LOGO_SMALL_IMG_PATH = './static/img/what2eat-logo-small.png'
+LOGO_TITLE_IMG_PATH = './static/img/what2eat-word-logo-small.png'
 DEFAULT_ADDRESS_INFO_LIST = ["강남구 삼성동", 127.0567474, 37.5074423]
 
 DINER_REVIEW_AVG = 3.2
 
+DATA_PATH = './data/seoul_data/whatToEat_DB_seoul_diner_202400902.csv'
 
 # Kakao API settings
 KAKAO_API_URL = "https://dapi.kakao.com/v2/local/search/keyword.json"
--- src/utils/data_loading.py
+++ src/utils/data_loading.py
@@ -2,11 +2,12 @@ import pandas as pd
 from PIL import Image
 import ast
 import streamlit as st
+from config.constants import DATA_PATH
 
 @st.cache_data
 def load_excel_data(logo_img_path, logo_small_img_path):
     # Load the Excel data and create the DataFrame
-    df_diner = pd.read_csv('../data/seoul_data/whatToEat_DB_seoul_diner_202400902.csv')
+    df_diner = pd.read_csv(DATA_PATH)
     df_diner['diner_category_detail'].fillna('', inplace=True)
     df_diner["diner_menu"] = df_diner["diner_menu"].apply(ast.literal_eval)
 
