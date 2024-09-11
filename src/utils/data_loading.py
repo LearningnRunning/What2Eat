dd61ec6834ca6096c9443dd69a5d4e50f645a6ae
LearningnRunning <rocki.k1208@gmail.com>
2024-09-11 11:55:06 +0900

    FIX data
    종로구 및 몇몇 지역에서 카테고리가 안 맞는 점 해결

--- src/utils/data_loading.py
+++ src/utils/data_loading.py
@@ -1,13 +1,23 @@
-import pandas as pd
-from PIL import Image
 import ast
+import glob
+from PIL import Image
+import pandas as pd
 import streamlit as st
+
+
 from config.constants import DATA_PATH
 
 @st.cache_data
 def load_excel_data(logo_img_path, logo_small_img_path):
-    # Load the Excel data and create the DataFrame
-    df_diner = pd.read_csv(DATA_PATH)
+    # Get the first CSV file in the directory
+    csv_files = glob.glob(DATA_PATH)
+    if not csv_files:
+        raise FileNotFoundError(f"No CSV files found in {DATA_PATH}")
+    
+    first_csv_file = csv_files[0]
+
+    # Load the CSV data and create the DataFrame
+    df_diner = pd.read_csv(first_csv_file)
     df_diner['diner_category_detail'].fillna('', inplace=True)
     df_diner["diner_menu"] = df_diner["diner_menu"].apply(ast.literal_eval)
 
