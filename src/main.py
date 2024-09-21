6428a242613a77d1dc0a47fd3c50a0bbe232c701
LearningnRunning <rocki.k1208@gmail.com>
2024-09-21 17:11:59 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ from config.constants import LOGO_IMG_PATH, LOGO_SMALL_IMG_PATH, LOGO_TITLE_IMG_
 st.set_page_config(page_title="머먹?", page_icon=LOGO_SMALL_IMG_PATH, layout="wide")
 df_diner, banner_image, icon_image, kakao_guide_image = load_static_data(LOGO_IMG_PATH, LOGO_TITLE_IMG_PATH, GUIDE_IMG_PATH)
 df_diner.rename(columns={'index': 'diner_idx'}, inplace=True)
-algo_knn, trainset_knn, user_item_matrix, user_similarity_df = load_model()
+# algo_knn, trainset_knn, user_item_matrix, user_similarity_df = load_model()
 
 # 아바타 선택 및 초기 메시지
 avatar_style, seed = choice_avatar()
@@ -82,37 +82,37 @@ if len(df_geo_filtered):
     df_geo_filtered_radius = df_geo_filtered_radius[df_geo_filtered_radius['real_good_review_cnt'].notna()]
     df_geo_filtered_real_review = df_geo_filtered_radius.query(f"(diner_review_avg >= diner_review_avg) and (real_good_review_cnt >= 5)")
 
-    search_option = st.radio("검색 방법을 선택하세요", ('카테고리로 찾기', '메뉴로 찾기', '추천 받기'))
+    search_option = st.radio("검색 방법을 선택하세요", ('카테고리로 찾기', '메뉴로 찾기')) #, '추천 받기'
     diner_nearby_cnt = len(df_geo_filtered)
     if search_option == '메뉴로 찾기':
         menu_search = st.text_input("찾고 싶은 메뉴를 입력하세요")
         if menu_search:
             df_menu_filtered = df_geo_filtered_real_review[df_geo_filtered_real_review.apply(lambda row: search_menu(row, menu_search), axis=1)]
             display_results(df_menu_filtered, diner_nearby_cnt, radius_distance)
-    elif search_option == '추천 받기':
-        kakao_id = st.text_input("카카오맵의 닉네임을 알려주시면 리뷰를 남긴 기반으로 추천을 해드려요.")
-        st.image(kakao_guide_image, width=300)
-        # # 사용자-아이템 매트릭스에 사용자가 있는지 확인
-        # if kakao_id in user_item_matrix.index:
-        #     # 추천 아이템 목록 생성 (기존 사용자)
-        #     recommended_items_df = recommend_items(
-        #         kakao_id, user_item_matrix, user_similarity_df, num_recommendations=50
-        #     )
-        # else:
-            # print(f"사용자 {kakao_id}가 데이터에 존재하지 않습니다.")
-        # 신규 사용자에 대한 추천 생성 (KNN 기반)
-        recommended_items_df = recommend_items_model(
-            kakao_id, algo_knn, trainset_knn, num_recommendations=200
-        )        
-        df_geo_filtered = df_geo_filtered[(df_geo_filtered['real_good_review_cnt'] > 4) & (df_geo_filtered['distance'] <= radius_kilometers)]
-        # 추천 결과에 위치 정보 병합
-        recommended_items_df = pd.merge(recommended_items_df, df_geo_filtered, on='diner_idx', how='right')
-        recommended_items_df = recommended_items_df[recommended_items_df['score'].notna()]
+    # elif search_option == '추천 받기':
+    #     kakao_id = st.text_input("카카오맵의 닉네임을 알려주시면 리뷰를 남긴 기반으로 추천을 해드려요.")
+    #     st.image(kakao_guide_image, width=300)
+    #     # # 사용자-아이템 매트릭스에 사용자가 있는지 확인
+    #     # if kakao_id in user_item_matrix.index:
+    #     #     # 추천 아이템 목록 생성 (기존 사용자)
+    #     #     recommended_items_df = recommend_items(
+    #     #         kakao_id, user_item_matrix, user_similarity_df, num_recommendations=50
+    #     #     )
+    #     # else:
+    #         # print(f"사용자 {kakao_id}가 데이터에 존재하지 않습니다.")
+    #     # 신규 사용자에 대한 추천 생성 (KNN 기반)
+    #     recommended_items_df = recommend_items_model(
+    #         kakao_id, algo_knn, trainset_knn, num_recommendations=200
+    #     )        
+    #     df_geo_filtered = df_geo_filtered[(df_geo_filtered['real_good_review_cnt'] > 4) & (df_geo_filtered['distance'] <= radius_kilometers)]
+    #     # 추천 결과에 위치 정보 병합
+    #     recommended_items_df = pd.merge(recommended_items_df, df_geo_filtered, on='diner_idx', how='right')
+    #     recommended_items_df = recommended_items_df[recommended_items_df['score'].notna()]
 
-        # 상위 N개의 추천 출력
-        num_final_recommendations = 20
-        final_recommendations = recommended_items_df.head(num_final_recommendations)
-        display_results(final_recommendations, diner_nearby_cnt, radius_distance)
+    #     # 상위 N개의 추천 출력
+    #     num_final_recommendations = 20
+    #     final_recommendations = recommended_items_df.head(num_final_recommendations)
+    #     display_results(final_recommendations, diner_nearby_cnt, radius_distance)
     
     else:
         my_chat_message("뭐 먹을겨?", avatar_style, seed)
