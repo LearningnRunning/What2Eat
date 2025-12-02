a538ba45a1bc682dbb176e89ebc3a1c5c977f39e
bohyun-shin <bohyun.shin@linecorp.com>
2025-12-02 18:05:58 +0900

    feat: call /rec/user/similar yamyam-ops api after finishing taste discovery step

--- src/pages/onboarding.py
+++ src/pages/onboarding.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import streamlit as st
 
+from utils.api import APIRequester
 from utils.auth import get_current_user
 from utils.category_manager import get_category_manager
 from utils.firebase_logger import get_firebase_logger
@@ -18,6 +19,7 @@ class OnboardingPage:
         self.onboarding_manager = get_onboarding_manager(app)
         self.category_manager = get_category_manager(app)
         self.min_ratings_required = 5  # 최소 평가 개수
+        self.api_requester = APIRequester(endpoint=st.secrets["API_URL"])
 
         # 온보딩 단계 초기화
         if "onboarding_step" not in st.session_state:
@@ -868,6 +870,34 @@ class OnboardingPage:
             st.session_state.total_rated_count = current_total
 
         if st.session_state.total_rated_count >= self.min_ratings_required:
+            # get most similar kakao_reviewer_id using /rec/user/similar
+            request_body = {
+                "liked_diner_ids": [int(diner_id.split("_")[-1]) for diner_id in st.session_state.restaurant_ratings.keys()],
+                "scores_of_liked_diner_ids": [score for score in st.session_state.restaurant_ratings.values()],
+            }
+            response = self.api_requester.post(
+                api_path="/rec/user/similar",
+                data=request_body,
+            ).json()
+
+            # update matched kakao_reviewer_id into db
+            kakao_reviewer_id = response["reviewer_id"]
+            firebase_uid = get_current_user()["localId"]
+            self.api_requester.put(
+                api_path=f"/users/{firebase_uid}",
+                # parameters should be reduced to include `kakao_reviewer_id` only
+                data={
+                    "name": "string",
+                    "email": "string",
+                    "display_name": "string",
+                    "photo_url": "string",
+                    "kakao_reviewer_id": str(kakao_reviewer_id)
+                },
+                params={
+                    "user_id_type": "firebase_uid"
+                }
+            )
+
             st.success(
                 f"✅ {st.session_state.total_rated_count}개 음식점 평가 완료! 다음 단계로 진행할 수 있습니다."
             )
